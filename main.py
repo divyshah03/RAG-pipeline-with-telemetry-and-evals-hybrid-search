69a0436ec25ebc90b18f77bf3c0bc763328f1f0b
Divy Shah <divyrshah3@gmail.com>
2026-08-28 22:31:39 -0500

    Fix Inngest throttle parameter from count to limit.

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ inngest_client = inngest.Inngest(
 @inngest_client.create_function(
     fn_id = "RAG: Ingest PDF",
     trigger = inngest.TriggerEvent(event = "rag/ingest_pdf"),
-    throttle = inngest.Throttle(count = 2, period = datetime.timedelta(minutes = 1)),
+    throttle = inngest.Throttle(limit = 2, period = datetime.timedelta(minutes = 1)),
     rate_limit = inngest.RateLimit(limit = 2, period = datetime.timedelta(hours = 4), key = "event.data.source_id")
 )
 
